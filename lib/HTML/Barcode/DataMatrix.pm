0045a9c67345de3c6f696b1e4db191048c38e17a
Mark A. Stratman <stratman@gmail.com>
2011-07-03 00:47:06 -0500

    Initial commit

---
+++ lib/HTML/Barcode/DataMatrix.pm
@@ -0,0 +1,111 @@
+package HTML::Barcode::DataMatrix;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+HTML::Barcode::DataMatrix - The great new HTML::Barcode::DataMatrix!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use HTML::Barcode::DataMatrix;
+
+    my $foo = HTML::Barcode::DataMatrix->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Mark A. Stratman, C<< <stratman at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-html-barcode-datamatrix at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=HTML-Barcode-DataMatrix>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc HTML::Barcode::DataMatrix
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=HTML-Barcode-DataMatrix>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/HTML-Barcode-DataMatrix>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/HTML-Barcode-DataMatrix>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/HTML-Barcode-DataMatrix/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Mark A. Stratman.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of HTML::Barcode::DataMatrix
